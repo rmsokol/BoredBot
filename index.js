8497e707f62a677a74e8d4090c739da6993d87b1
Rachel Sokol <50994670+rmsokol@users.noreply.github.com>
2023-11-16 05:01:48 -0500

    Add file for initial repo creation

---
+++ index.js
@@ -0,0 +1,14 @@
+
+document.getElementById("get-activity").addEventListener("click", function() {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("activity").textContent = data.activity
+      document.getElementById("title").textContent = "🦾 HappyBot🦿"
+      document.getElementById("reset").innerHTML = `<h4> Resetting in 5 seconds!</h4>`;
+      document.body.classList.add("fun")
+      document.getElementById("activity").classList.add("scared")
+      setTimeout(() => { location.reload() }, 5000)
+    })
+})
+
